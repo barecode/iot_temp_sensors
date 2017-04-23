44b802f2f5896551f9549e5d191244d239fde2ac
barecode <ppzq2k3@aol.com>
2017-04-23 04:30:16 +0000

    Add some comments to explain each step

--- iot/configure_nodejs.sh
+++ iot/configure_nodejs.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+# Check to see if nodejs is installed
 if [[ -z `which nodejs` ]]; then
+    # If nodejs isn't installed, then install it
     apt-get update
     apt-get install -y nodejs nodejs-legacy 
 fi
+
+# Check to see if npm is installed
 if [[ -z `which npm` ]]; then
+    # If npm isn't installed, then install it
     apt-get install -y npm
 fi
+
+# Check to see if pm2 is installed
 if [[ -z `which pm2` ]]; then
+    # If pm2 isn't installed, then install it
     npm install -g pm2 
 fi
+
+# Install prerequisite nodejs libraries
 npm install stat moment fs ds18x20 ibmiotf
+
+
