abd4a1a5b31c6296eebb04282397bd62b82dc567
barecode <ppzq2k3@aol.com>
2017-04-23 04:40:12 +0000

    Add git ignore for node_modules

--- iot/configure_nodejs.sh
+++ iot/configure_nodejs.sh
@@ -1,22 +1,41 @@
 #!/bin/bash
 
+# Update function, only update once per run
+updated=
+function apt-get-update() {
+   if [[ $updated ]]; then
+       echo "already updated"
+   else
+       updated=done
+       echo "updating"
+       #apt-get update
+   fi
+}
+
 # Check to see if nodejs is installed
 if [[ -z `which nodejs` ]]; then
     # If nodejs isn't installed, then install it
-    apt-get update
+    apt-get-update
     apt-get install -y nodejs nodejs-legacy 
+else
+    echo "nodejs already installed"
 fi
 
 # Check to see if npm is installed
 if [[ -z `which npm` ]]; then
     # If npm isn't installed, then install it
+    apt-get-update
     apt-get install -y npm
+else
+    echo "npm already installed"
 fi
 
 # Check to see if pm2 is installed
 if [[ -z `which pm2` ]]; then
     # If pm2 isn't installed, then install it
     npm install -g pm2 
+else
+    echo "pm2 already installed"
 fi
 
 # Install prerequisite nodejs libraries
