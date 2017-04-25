6a1f01dd815bf8054a30a85acafa233c8b397c6d
barecode <ppzq2k3@aol.com>
2017-04-25 22:24:07 +0000

    Augment read to handle multiple connected DS18B20 devices

--- read_DS18B20.py
+++ read_DS18B20.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import glob
 import time
@@ -6,20 +7,22 @@ os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 
 base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')[0]
-device_file = device_folder + '/w1_slave'
+device_folders = glob.glob(base_dir + '28-*')
+devices = map(lambda x: os.path.basename(x), device_folders)
+device_files = map(lambda x: x + '/w1_slave', device_folders)
 
-def read_temp_raw():
+def read_temp_raw(device):
+    device_file = base_dir + device + '/w1_slave'
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
-def read_temp():
-    lines = read_temp_raw()
+def read_temp(device):
+    lines = read_temp_raw(device)
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = read_temp_raw()
+        lines = read_temp_raw(device)
     equals_pos = lines[1].find('t=')
     if equals_pos != -1:
         temp_string = lines[1][equals_pos+2:]
@@ -28,6 +31,8 @@ def read_temp():
         return temp_c, temp_f
 
 while True:
-	print(read_temp())
-	time.sleep(1)
+    print datetime.datetime.now()
+    for device in devices:
+	print(device, read_temp(device))
+    time.sleep(1)
 
