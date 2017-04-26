6bb7274a03393b9be0c0d44bb36b4f7b9f1a8303
barecode <ppzq2k3@aol.com>
2017-04-26 03:48:51 +0000

    Actually get the startMonitor script to work... need to kill pm2 in order to force the environment ariables into its address space... hacky but functional

--- startMonitor.sh
+++ startMonitor.sh
@@ -2,4 +2,10 @@
 
 source secure.sh
 echo "Starting monitor with AUTH_TOKEN=$AUTH_TOKEN"
-pm2 start iot/raspi3monitor.js
+#nodejs iot/raspi3monitor.js
+
+# Need to kill pm2 and start with the new environment variable in pm2's address space
+# This is hacky, but works
+pm2 kill
+
+AUTH_TOKEN=$AUTH_TOKEN pm2 start iot/raspi3monitor.js
